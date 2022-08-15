1197ecc646df73cf1ec9955e3e3bd27bc5671785
Your Name <you@example.com>
2022-08-15 22:10:47 +0700

    btvn

---
+++ btvn7.js
@@ -0,0 +1,12 @@
+let greeting = 'Hello. My name is John'
+let letter_1 = greeting.charAt(8);
+console.log('answer_1:', letter_1)
+
+let str1 = 'The morning is upon us'
+let letter_2 = str1.slice(11,22)
+console.log('answer_2:', letter_2)
+
+let str2 = 'Hello'
+let str3 = 'World'
+let letter_3 = str2.concat(str3)
+console.log('answer_3:', letter_3)
